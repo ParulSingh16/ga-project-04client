6aa9ccd928a23fe4e893d950eefb7e1d4a19be12
Parul Singh <parul@paruls-air.home>
2023-01-25 20:28:41 +0000

    style provider cards

--- ga-project-04client/src/components/common/ProviderCard.js
+++ ga-project-04client/src/components/common/ProviderCard.js
@@ -17,20 +17,89 @@ export default function ProviderCard({ name, id, policyName, productName }) {
           sx={{ maxHeight: 345, objectFit: "contain" }}
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {policyName}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            {productName}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            {`£${(Math.random(50, 100) * 100).toFixed(0)}`}
-          </Typography>
+          <div
+            style={{
+              borderRadius: "8px",
+              textAlign: "center",
+              color: "black",
+              fontSize: "20px",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "30px" }}
+            >
+              {policyName}
+            </Typography>
+          </div>
+          <div
+            style={{
+              backgroundColor: "rgb(255 176 13)",
+              borderRadius: "8px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "20px" }}
+            >
+              {productName}
+            </Typography>
+          </div>
+          <div
+            style={{
+              backgroundColor: "rgb(255 176 13)",
+              borderRadius: "8px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "20px" }}
+            >
+              {name}
+            </Typography>
+          </div>
+          <div
+            style={{
+              backgroundColor: "rgb(255 176 13)",
+              borderRadius: "8px",
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ fontSize: "20px" }}
+            >
+              {`£${(Math.random(50, 100) * 100).toFixed(0)}`}
+            </Typography>
+          </div>
         </CardContent>
-        <button style={{ marginLeft: "40%", marginBottom: "10px" }}>BUY</button>
+        <button
+          style={{
+            marginLeft: "20%",
+            fontSize: "18px",
+            borderRadius: "6px",
+            border: "none",
+            marginBottom: "10px",
+            width: "200px",
+            backgroundColor: "#397dd7",
+            color: "white",
+          }}
+        >
+          BUY
+        </button>
       </CardActionArea>
     </Card>
   );
